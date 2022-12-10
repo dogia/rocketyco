e9f106c996d3dd68be175763c904d823b4a5e152
Dogia <danielosorioorozco@gmail.com>
2022-12-10 00:18:55 -0500

    Company app

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+import os
 from fastapi import FastAPI
 from dotenv import load_dotenv
+
+from apps.CompanyApp import CompanyApp
 
 
 load_dotenv()
 
-RocketyApi = FastAPI()
+RocketyApi = FastAPI(
+    title=os.getenv('TITLE') + " | Company",
+    debug=(os.getenv('DEBUG') == 'True')
+)
+
+RocketyApi.mount('/company', CompanyApp)
