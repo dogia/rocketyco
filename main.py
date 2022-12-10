25d4246d9d4a9acc885707be1ff77296bea3b19d
Dogia <danielosorioorozco@gmail.com>
2022-12-10 03:19:11 -0500

    Renombrado a app para uso comodo de uvicorn

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from apps.CompanyApp import CompanyApp
 
 load_dotenv()
 
-RocketyApi = FastAPI(
+app = FastAPI(
     title=os.getenv('TITLE') + " | Company",
     debug=(os.getenv('DEBUG') == 'True')
 )
 
-RocketyApi.mount('/company', CompanyApp)
+app.mount('/company', CompanyApp)
