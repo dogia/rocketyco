ac0aae026aca38fe72603390615df717b2e5626f
Dogia <danielosorioorozco@gmail.com>
2022-12-10 00:37:34 -0500

    Aceptar descripción de vacantes

--- models/CompanyModel.py
+++ models/CompanyModel.py
@@ -1,9 +1,13 @@
-from typing import Optional
+from typing import List, Optional
 from uuid import uuid4
 from pydantic import UUID4, BaseModel, Field
+
+from models.VacancyModel import VacancyModel
 
 
 class CompanyModel(BaseModel):
     CompanyId: UUID4 = Field(default_factory=uuid4)
     CompanyName: str
     CompanyWebsite: Optional[str]
+
+    Vacancies: Optional[List[VacancyModel]]
